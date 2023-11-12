4c4b13a38fde04e0ca7fc97d4f7dbbbaab9be56d
alsruawo <144800432+alsruawo@users.noreply.github.com>
2023-11-13 01:11:37 +0900

    Delete lib directory

--- lib/api/api.dart
+++
@@ -1,7 +0,0 @@
-class API{
-  static const hostConnect  = "http://192.168.0.4/api_new_members";
-  static const hostConnectUser = "$hostConnect/user";
-
-  static const signup = "$hostConnect/user/signup.php";
-  static const validateEmail = "$hostConnect/user/validate_email.php";
-}
