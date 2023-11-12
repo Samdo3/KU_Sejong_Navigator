2f53487ada5b38c6dedda27a6efcdbd9d61d975b
Samdo3 <clcleh123@gmail.com>
2023-11-13 01:46:39 +0900

    <Map추가>-map.dart를 따로 실행해야함
    - gradle\gradle.properties에 추가(시크릿키)
    
    MAPBOX_DOWNLOADS_TOKEN=(secret키)
    
    - app\src\main\res\values\R.strings.xml에 추가(공용키)
    
    - app\src\main\java\com\example\maptest\MainActivity.java 생성(앱 실행시 위치 권한 물어봄)
    
    - .gitignore에 추가
    # access_token
    access_tokens.dart
    
    - lib\access_tokens.dart 추가
    
    - build.gradle에 최소 버전 추가

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,5 +1,5 @@
 class API{
-  static const hostConnect  = "http://192.168.0.4/api_new_members";
+  static const hostConnect  = "http://192.168.50.119/api_new_members";
   static const hostConnectUser = "$hostConnect/user";
 
   static const signup = "$hostConnect/user/signup.php";
