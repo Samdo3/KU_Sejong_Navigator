f37086a498e6d1da83600dc44c2a30262b99e3fe
Samdo3 <clcleh123@gmail.com>
2023-11-18 03:21:21 +0900

    경로 중간 턴 포인트에 마크 추가

--- android/app/src/main/kotlin/com/example/getx_mysql_tutorial/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/getx_mysql_tutorial/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.getx_mysql_tutorial
 
-//import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
+
+class MainActivity: FlutterActivity() {
 }
--- lib/map/setup.dart
+++ lib/map/setup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'access_tokens.dart'; //access_tokens.dart 파일을 import
 import 'package:http/http.dart' as http;
@@ -46,6 +47,11 @@ class _MapScreenState extends State<MapScreen> {
 
         try {
           RouteData routeData = await fetchRoute();
+          var markerImage=await loadMarkerImage();
+
+          // 마커 이미지 추가
+          controller.addImage('marker',markerImage);
+
           // LineLayer를 사용하여 Polyline을 추가
           controller.addLine(LineOptions(
             geometry: route,
@@ -57,11 +63,13 @@ class _MapScreenState extends State<MapScreen> {
           for (int i = 0; i < routeData.turnPoints.length; i++) {
             controller.addSymbol(SymbolOptions(
               geometry: routeData.turnPoints[i],
-              iconImage: 'your-turn-icon-image', // 사용자가 제공하는 아이콘 이미지로 변경
-              iconSize: 2.0,
+              iconImage: 'marker', // 사용자가 제공하는 아이콘 이미지로 변경
+              iconSize: 0.5,
               textField: 'Turn ${i + 1}',
               textOffset: Offset(0, 2),
             ));
+            // 추가된 마커
+
           }
         } catch (error) {
           print("Error adding polyline: $error");
@@ -70,23 +78,30 @@ class _MapScreenState extends State<MapScreen> {
     );
   }
 
+  //마커 이미지 경로 인식
+  Future<Uint8List> loadMarkerImage() async {
+    var byteData = await rootBundle.load("assets/images/red_marker.png");
+    return byteData.buffer.asUint8List();
+  }
+
   Future<RouteData> fetchRoute() async {
     try {
       LatLng origin = LatLng(36.610866, 127.287018); // 출발지 좌표 (원하는 좌표로 교체하세요)
-      LatLng destination = LatLng(
-          36.611321, 127.288271); // 도착지 좌표 (원하는 좌표로 교체하세요)
+      LatLng destination = LatLng(36.608858, 127.289094); // 도착지 좌표 (원하는 좌표로 교체하세요)
 
       String routeResponse = (await http.get(
         Uri.parse(
           "https://api.mapbox.com/directions/v5/mapbox/walking/"
               "${origin.longitude},${origin.latitude};"
               "${destination.longitude},${destination.latitude}?"
-              "access_token=${AppConstants.mapBoxAccessToken}",
+              "access_token=${AppConstants.mapBoxAccessToken}&steps=true",
         ),
       )).body;
 
+      print("Route Response: $routeResponse");
       var decodedResponse = json.decode(routeResponse);
 
+      print("Decoded Response: $decodedResponse");
       // 루트 지오메트리 디코딩 예제
       List<PointLatLng> result = PolylinePoints()
           .decodePolyline(decodedResponse['routes'][0]['geometry'])
@@ -94,8 +109,16 @@ class _MapScreenState extends State<MapScreen> {
           .toList();
 
       // PointLatLng를 LatLng로 변환
-      List<LatLng> route = result.map((PointLatLng point) =>
+      route = result.map((PointLatLng point) =>
           LatLng(point.latitude, point.longitude)).toList();
+
+      List<dynamic> steps = decodedResponse['routes'][0]['legs'][0]['steps'] ?? [];
+
+      List<LatLng> turnPoints=steps.map((step) => LatLng(
+        step['maneuver']['location'][1],
+        step['maneuver']['location'][0],
+      )).toList();
+
 
 
       return RouteData(route: route, turnPoints: turnPoints);
@@ -104,14 +127,6 @@ class _MapScreenState extends State<MapScreen> {
       // 에러 핸들링 또는 사용자에게 알림을 보여주는 등의 추가 작업이 가능합니다.
       throw error; // 에러를 호출한 곳으로 전파
     }
-  }
-  List<LatLng> getTurnPoints(Map<String, dynamic> decodedResponse) {
-    List<dynamic> steps = decodedResponse['routes'][0]['legs'][0]['steps'];
-
-    return steps.map((step) => LatLng(
-      step['maneuver']['location'][1],
-      step['maneuver']['location'][0],
-    )).toList();
   }
 
 
