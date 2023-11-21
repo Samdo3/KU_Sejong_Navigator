f7ad799bb3d85c08fc2c02085985933991932581
Samdo3 <clcleh123@gmail.com>
2023-11-22 00:57:59 +0900

    겹치는부분 합침

--- lib/user/pages/main_screen.dart
+++ lib/user/pages/main_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_mysql_tutorial/map/map.dart';
-import 'package:getx_mysql_tutorial/user/pages/timetable.dart';
+import 'package:getx_mysql_tutorial/map/navigator.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -13,32 +15,56 @@ class MainScreen extends StatelessWidget {
         title: Text('Main Screen'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => MyMap());
-              },
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size(120, 50), // Set the desired size for the button
-              ),
-              child: Text('경로 안내'),
-            ),
-            SizedBox(height: 20), // Add some spacing between the buttons
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => Timetable());
-              },
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size(120, 50), // Set the same size for the button
-              ),
-              child: Text('시간표'),
-            ),
-            SizedBox(height: 20),
-          ],
+        child:ElevatedButton(
+          onPressed: (){
+            _AskLocationPermission();
+          },
+          child: Text('경로 안내'),
         ),
       ),
+    );
+  }
+  void _AskLocationPermission() async {
+    // 위치 권한이 부여되었는지 확인
+    bool isLocationPermissionGranted = await _checkLocationPermission();
+
+    if (isLocationPermissionGranted) {
+      Get.to(() => MapScreen());
+    } else {
+      // 위치 권한이 부여되지 않았을 경우 메시지 표시 또는 권한 요청
+      showToast('위치 권한이 부여되지 않았습니다.');
+      _requestLocationPermission();
+    }
+  }
+
+  Future<bool> _checkLocationPermission() async {
+    // 위치 권한이 부여되었는지 확인
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      return true;
+    } else {
+      // 권한이 부여되지 않았다면 요청
+      return await _requestLocationPermission();
+    }
+  }
+
+  Future<bool> _requestLocationPermission() async {
+    // 위치 권한 요청
+    LocationPermission permission = await Geolocator.requestPermission();
+    return permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse;
+  }
+
+  void showToast(String message) {  //토스트 형식
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0,
     );
   }
 }
